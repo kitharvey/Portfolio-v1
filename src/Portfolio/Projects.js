94d2b67b337ca27ab547feac9d51508c4f031784
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2020-06-21 22:11:24 +0800

    4th proj added

--- src/Portfolio/Projects.js
+++ src/Portfolio/Projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import sort from '../photo/sort.jpg'
 import crow from '../photo/crow.jpg'
 import path from '../photo/path.jpg'
+import fx from '../photo/fx.jpg'
 
 export default function Projects(){
     const style = {
@@ -10,7 +11,7 @@ export default function Projects(){
     return(
         <div id="projects">
             <p style={style}>projects</p>
-            <div className="proj-one" >
+            <div className="proj" >
                 <a className = "details" href="https://kitharvey.github.io/Sort-Animation/" target="__blank">
                     <h1>Sort Visualizer</h1>
                     <small>Sorting Algorithms Visualizer Applet</small>
@@ -20,7 +21,7 @@ export default function Projects(){
                 </div>
                 
             </div>
-            <div className="proj-two" >
+            <div className="proj" >
                 <a className = "details" href="https://kitharvey.github.io/coffee-ecommerce/" target="__blank">
                     <h1>Cockcrow</h1>
                     <small>Coffee Company E-commerce Website</small>
@@ -30,13 +31,23 @@ export default function Projects(){
                 </div>
                 
             </div>
-            <div className="proj-three" >
+            <div className="proj" >
                 <a className = "details" href="https://kitharvey.github.io/Pathfinder/" target="__blank">
                     <h1>Pathfinder</h1>
                     <small>A* Search Algorithm Visualizer</small>
                 </a>
                 <div className = "dropdown">
                     <img src={path} alt="pathfinder screenshot"/>
+                </div>
+                
+            </div>
+            <div className="proj" >
+                <a className = "details" href="https://kitharvey.github.io/fx-daily-forecast/" target="__blank">
+                    <h1>FX Daily Forecast</h1>
+                    <small>FX API Data Presentation</small>
+                </a>
+                <div className = "dropdown">
+                    <img src={fx} alt="fx screenshot"/>
                 </div>
                 
             </div>
