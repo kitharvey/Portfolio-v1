9c39db1001ba1260e92ff41c7fee6dbbf585e87a
Kit Harvey V. Caubalejo <54163221+kitharvey@users.noreply.github.com>
2021-01-06 23:24:43 +0800

    Updates

---
+++ precache-manifest.0af68abbd0f0964ded9deaeb806080a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa5715cfae545b3f538af23fec020e2",
+    "url": "/Portfolio-v1/index.html"
+  },
+  {
+    "revision": "8eedb1fac55dc360f544",
+    "url": "/Portfolio-v1/static/css/main.b7ff3559.chunk.css"
+  },
+  {
+    "revision": "17b5a7cc7cdd39a89984",
+    "url": "/Portfolio-v1/static/js/2.9e5113d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio-v1/static/js/2.9e5113d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eedb1fac55dc360f544",
+    "url": "/Portfolio-v1/static/js/main.b2f7d60d.chunk.js"
+  },
+  {
+    "revision": "b90b588560edaf3a6cef",
+    "url": "/Portfolio-v1/static/js/runtime-main.cf48a598.js"
+  },
+  {
+    "revision": "2407340528d464c86c656d4e603d7fa9",
+    "url": "/Portfolio-v1/static/media/crow.24073405.jpg"
+  },
+  {
+    "revision": "64ac50b222f44878c3ad8490d9412dff",
+    "url": "/Portfolio-v1/static/media/fx.64ac50b2.jpg"
+  },
+  {
+    "revision": "3c645a00bf113887c3405991cabc7c63",
+    "url": "/Portfolio-v1/static/media/gifascii.3c645a00.jpg"
+  },
+  {
+    "revision": "174589dd3965c2a418a8b586d21472a2",
+    "url": "/Portfolio-v1/static/media/path.174589dd.jpg"
+  },
+  {
+    "revision": "21b71886935ccba7455dff936754d76b",
+    "url": "/Portfolio-v1/static/media/resume.21b71886.pdf"
+  },
+  {
+    "revision": "383bcd5eda9b8fc5b08281f119facbc1",
+    "url": "/Portfolio-v1/static/media/sort.383bcd5e.jpg"
+  },
+  {
+    "revision": "1121f8f4c7943a4d598b6ccaf6131340",
+    "url": "/Portfolio-v1/static/media/test.1121f8f4.jpg"
+  }
+]);
